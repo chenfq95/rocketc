7392c85bfd5db4678b608817bc3f1c6eac4e722b
chenfq <chenfq95@foxmail.com>
2025-12-30 03:00:28 +0800

    add react components

---
+++ packages/rocketc-react-ui/vite.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import dts from 'unplugin-dts/vite';
+import packageJson from './package.json';
+import tailwindcss from '@tailwindcss/vite';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// https://vite.dev/config/
+export default defineConfig({
+	plugins: [
+		react(),
+		dts({
+			tsconfigPath: './tsconfig.lib.json',
+			bundleTypes: true,
+		}),
+		tailwindcss(),
+	],
+	build: {
+		sourcemap: true,
+		minify: false,
+		cssMinify: false,
+		lib: {
+			entry: 'src/index.ts',
+			formats: ['es'],
+			fileName: 'index',
+			cssFileName: 'style',
+		},
+		rollupOptions: {
+			external: [
+				'react/jsx-runtime',
+				...Object.keys(packageJson.dependencies || {}),
+				...Object.keys(packageJson.peerDependencies || {}),
+			],
+		},
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
+});
